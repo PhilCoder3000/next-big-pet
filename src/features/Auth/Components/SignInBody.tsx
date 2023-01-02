1f813ecfc8d573b35a64c5c61209908cf6733698
Philipp Zakharov <philippzakharov@gamil.com>
2023-01-02 21:20:07 +0300

    Add useForm

--- src/features/Auth/Components/SignInBody.tsx
+++ src/features/Auth/Components/SignInBody.tsx
@@ -2,24 +2,37 @@ import React, { useState } from 'react';
 import { BaseButton } from '../../../shared/buttons/BaseButton';
 import { BaseTextField } from '../../../shared/fields/BaseTextField';
 import { PasswordTextField } from '../../../shared/fields/PasswordTextField';
+import { useForm } from '../../../shared/hooks/useForm';
 import { useSignIn } from '../hooks/useSignIn';
 
 interface SignInBodyProps {}
 
 export function SignInBody({}: SignInBodyProps) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.name === 'email') {
-      setEmail(e.target.value);
-    }
-    if (e.target.name === 'password') {
-      setPassword(e.target.value);
-    }
+  // const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.name === 'email') {
+  //     setEmail(e.target.value);
+  //   }
+  //   if (e.target.name === 'password') {
+  //     setPassword(e.target.value);
+  //   }
+  // };
+
+  const onSubmit = (value: any) => {
+    console.log(value);
   };
 
-  const { login } = useSignIn(email, password);
+  const { value, errors, changeHandler, submitHandler } = useForm(
+    { email: '', password: '' },
+    onSubmit,
+    { email: { isRequired: { errorMessage: 'is not email'} }, password: { isRequired: true } },
+    );
+  console.log('🚀 ~ file: SignInBody.tsx:28 ~ SignInBody ~ errors', errors);
+  const { email, password } = value;
+
+  // const { login } = useSignIn(email, password);
 
   return (
     <>
@@ -36,7 +49,7 @@ export function SignInBody({}: SignInBodyProps) {
         name="password"
         onChange={changeHandler}
       />
-      <BaseButton className="ml-auto mt-auto" onClick={login}>
+      <BaseButton className="ml-auto mt-auto" onClick={submitHandler}>
         Login
       </BaseButton>
     </>
---
+++ src/shared/hooks/useForm.ts
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+type ValidationRuleObject = { errorMessage: string };
+
+type ValidationRule = boolean | ValidationRuleObject;
+
+type Validation<T> = {
+  [key in keyof T]?: {
+    isRequired?: ValidationRule;
+    isEmail?: ValidationRule;
+  };
+};
+
+interface InitialValue {
+  [key: string]: string | number;
+}
+
+type Errors = Record<keyof InitialValue, boolean | string>;
+
+export function useForm<T extends InitialValue>(
+  initialValue: T,
+  onSubmit: (value: T) => void,
+  validation?: Validation<T>,
+) {
+  const [value, setValue] = useState<T>(initialValue);
+  const [errors, setErrors] = useState(() => getDefaultErrors(initialValue));
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value: inputValue } = e.target;
+    if (name in value) {
+      setValue((prev) => ({
+        ...prev,
+        [name]: inputValue,
+      }));
+    }
+  };
+
+  const submitHandler = () => {
+    if (validation) {
+      const { isValid, invalidName, errors } = validateValue(value, validation);
+
+      if (isValid) {
+        onSubmit(value);
+      } else {
+        setErrors(errors);
+        document
+          .getElementsByName(invalidName)[0]
+          .scrollTo({ behavior: 'smooth' });
+      }
+    } else {
+      onSubmit(value);
+    }
+  };
+
+  return {
+    value,
+    errors,
+    changeHandler,
+    submitHandler,
+  };
+}
+
+const validateValue = (
+  value: InitialValue,
+  validation: Validation<InitialValue>,
+) => {
+  let invalidName = '';
+  let errors = {} as Errors;
+  Object.keys(validation).forEach((key) => {
+    const validationRules = validation[key]!;
+    if (validationRules.isRequired) {
+      if (typeof value[key] === 'string') {
+        if ((value[key] as string).trim() === '') {
+          invalidName = invalidName || key;
+          if (typeof validationRules.isRequired === 'boolean') {
+            errors[key] = true;
+          } else {
+            errors[key] = validationRules.isRequired.errorMessage
+          }
+        }
+      }
+    }
+  });
+  return { isValid: !Object.keys(errors).length, invalidName, errors };
+};
+
+const getDefaultErrors = (value: InitialValue) => {
+  const errors = {} as Errors;
+
+  Object.keys(value).forEach((key) => (errors[key] = false));
+
+  return errors;
+};
